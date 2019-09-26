f649067150c058cb2541322ca3d066b2b41a4207
mahmoudselmy06@gmail.com <mahmoud_sam_06>
2019-09-26 20:52:30 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:admin_flutter_app'
+    '/screens/admin.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Admin(),
+  ));
+}
